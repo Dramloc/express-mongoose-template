54a55871c94fd2ab839dadf847fca7f1ea2763d9
Antoine Colmard <a.colmard@gmail.com>
2018-06-05 00:31:10 +0200

    :hammer: Use http-status for error status

--- src/middleware/error-handler/index.js
+++ src/middleware/error-handler/index.js
@@ -1,10 +1,12 @@
+import httpStatus from 'http-status';
+
 export default function errorHandler() {
   return (err, req, res, next) => {
     if (!err) {
       return next();
     }
-    const status = err.status || 500;
-    const message = err.message || 'Unexpected error';
+    const status = err.status || httpStatus.INTERNAL_SERVER_ERROR;
+    const message = err.message || httpStatus['500_MESSAGE'];
     return res.status(status).json({
       status,
       message,
--- src/middleware/not-found/index.js
+++ src/middleware/not-found/index.js
@@ -1,6 +1,8 @@
+import httpStatus from 'http-status';
+
 export default function notFound() {
   return (req, res) => {
-    const status = 404;
+    const status = httpStatus.NOT_FOUND;
     const message = `Cannot ${req.method} ${req.url}`;
     return res.status(status).json({
       status,
