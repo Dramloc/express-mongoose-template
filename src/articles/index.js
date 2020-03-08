3cee1f17101ea24b9bedd6ffecc3f2ca50d0daa8
Antoine Colmard <a.colmard@gmail.com>
2020-03-09 00:47:42 +0100

    :bulb: Add rest helpers documentation

--- src/articles/index.js
+++ src/articles/index.js
@@ -7,37 +7,37 @@ const router = express.Router();
 
 router
   .route("/")
-  .get(rest.find({ find: req => crud.find(Article, req.query) }))
+  .get(rest.find({ handler: req => crud.find(Article, req.query) }))
   .post(
-    rest.bind({ bind: (document, req) => crud.create(Article, req.body) }),
-    rest.validate({ validate: crud.validate }),
-    rest.save({ save: crud.save, isNew: true })
+    rest.bind({ handler: (_, req) => crud.create(Article, req.body) }),
+    rest.validate({ handler: crud.validate }),
+    rest.save({ handler: crud.save, isNew: true })
   );
 
-router.param("id", rest.validateParam({ validate: crud.isIdValid })).param(
+router.param("id", rest.validateParam({ handler: crud.isIdValid })).param(
   "id",
   rest.load({
-    load: (id, req) => crud.findById(Article, id, req.query),
+    handler: (id, req) => crud.findById(Article, id, req.query),
     modelName: Article.modelName
   })
 );
 
 router
   .route("/:id")
-  .get(rest.findById)
+  .get(rest.findById())
   .put(
     rest.bind({
-      bind: (document, req) => crud.update(document, req.body)
+      handler: (document, req) => crud.update(document, req.body)
     }),
-    rest.validate({ validate: crud.validate }),
-    rest.save({ save: crud.save, isNew: false })
+    rest.validate({ handler: crud.validate }),
+    rest.save({ handler: crud.save, isNew: false })
   )
   .patch(
     rest.bind({
-      bind: (document, req) => crud.patch(document, req.body)
+      handler: (document, req) => crud.patch(document, req.body)
     }),
-    rest.validate({ validate: crud.validate }),
-    rest.save({ save: crud.save, isNew: false })
+    rest.validate({ handler: crud.validate }),
+    rest.save({ handler: crud.save, isNew: false })
   )
   .delete(rest.remove({ remove: crud.remove }));
 
--- src/utils/rest.js
+++ src/utils/rest.js
@@ -1,18 +1,49 @@
 import Boom from "@hapi/boom";
 
-/** @type {<T> (options: { find: (req: import("express").Request) => [T[], number] | Promise<[T[], number]> }) => import("express").RequestHandler} */
-export const find = ({ find }) => async (req, res, next) => {
+/**
+ * Execute `handler` to retrieve a list of document and the total count of documents in this collection.
+ * It will then add the resulting collection document to the response header (`X-Total-Count` by default),
+ * and set the resulting list as the response body.
+ * @param {Object} options
+ * @param {(req: import("express").Request) => [T[], number] | Promise<[T[], number]>} options.handler The handler that finds a list of document and the document count in the collection
+ * @param {string} [options.totalCountHeader="X-Total-Count"] The header name to used for the total document count
+ * @returns {import("express").RequestHandler}
+ * @template T
+ * @example
+ * ```js
+ * router.route('/').get(find({ handler: (req) => findAllArticles(req.query) }))
+ * ```
+ * @example
+ * ```js
+ * router.route('/').get(find({ handler: (req) => findAllArticles(req.query), totalCountHeader: "Pagination-Count" }))
+ * ```
+ */
+export const find = ({ handler, totalCountHeader = "X-Total-Count" }) => async (
+  req,
+  res,
+  next
+) => {
   try {
-    const [list, totalCount] = await find(req);
-    res.set("X-Total-Count", String(totalCount));
+    const [list, totalCount] = await handler(req);
+    res.set(totalCountHeader, String(totalCount));
     return res.json(list);
   } catch (error) {
     return next(error);
   }
 };
 
-/** @type {(options: { validate: (value: string, req: import("express").Request) => boolean | Promise<boolean> }) => import("express").RequestParamHandler} */
-export const validateParam = ({ validate }) => async (
+/**
+ * Validate an express parameter with the given handler.
+ * If handler does not validate the parameter value, will return a 400.
+ * @param {Object} options
+ * @param {(value: string, req: import("express").Request) => boolean | Promise<boolean>} options.handler - The parameter validator
+ * @returns {import("express").RequestParamHandler}
+ * @example
+ * ```js
+ * router.param('id', validateParam({ handler: (value) => isParamaterValid(value) }))
+ * ```
+ */
+export const validateParam = ({ handler }) => async (
   req,
   res,
   next,
@@ -20,7 +51,7 @@ export const validateParam = ({ validate }) => async (
   name
 ) => {
   try {
-    const isValid = await validate(value, req);
+    const isValid = await handler(value, req);
     if (isValid) {
       return next();
     }
@@ -32,65 +63,244 @@ export const validateParam = ({ validate }) => async (
   }
 };
 
-/** @type {<T> (options: { load: (value: string, req: import("express").Request) => Promise<T>, modelName?: string }) => import("express").RequestParamHandler} */
-export const load = ({ load, modelName = "Document" }) => async (
-  req,
-  res,
-  next,
-  value,
-  name
-) => {
+/**
+ * Load a document in the express locals using the given handler.
+ * If the document cannot be found, returns a 404.
+ * The 404 message can be changed by changing the `modelName` parameter.
+ * The locals where the document is stored can be changed by specifying the `documentKey` parameter (`document` is used by default).
+ * @param {Object} options
+ * @param {(value: string, req: import("express").Request) => T | Promise<T>} options.handler The handler that loads the document given the param value
+ * @param {string} [options.modelName="Document"] The document model name (used for error message)
+ * @param {string} [options.documentKey="document"] The express local key where the document will be stored
+ * @returns {import("express").RequestParamHandler}
+ * @template T
+ * @example
+ * ```js
+ * router.param('id', load({ handler: (value) => findArticleById(value) }));
+ * ```
+ * @example
+ * ```js
+ * router.param('id', load({ handler: (value) => findArticleById(value), documentKey: "article" }));
+ * ```
+* @example
+ * ```js
+ * router.param('id', load({ handler: (value) => findArticleById(value), modelName: "Article" }));
+ * ```
+ */
+export const load = ({
+  handler,
+  modelName = "Document",
+  documentKey = "document"
+}) => async (req, res, next, value, name) => {
   try {
-    const document = await load(value, req);
+    const document = await handler(value, req);
     if (!document) {
       return next(
         Boom.notFound(`${modelName} with ${name} "${value}" not found.`)
       );
     }
-    res.locals.document = document;
+    res.locals[documentKey] = document;
     return next();
   } catch (error) {
     return next(error);
   }
 };
 
-/** @type {import("express").RequestHandler} */
-export const findById = (req, res) => res.json(res.locals.document);
+/**
+ * Outputs the express locals with given key.
+ * @param {Object} options
+ * @param {string} [options.documentKey="document"] The express local key where the document is stored
+ * @returns {import("express").RequestHandler}
+ * @example
+ * ```js
+ * router.param('id', load({ handler: (value) => findArticleById(value) }));
+ * router.router('/:id').get(findById());
+ * ```
+ * @example
+ * ```js
+ * router.param('id', load({ handler: (value) => findArticleById(value), documentKey: 'article' }));
+ * router.router('/:id').get(findById({ documentKey: 'article' }));
+ * ```
+ */
+export const findById = ({ documentKey = "document" } = {}) => (req, res) =>
+  res.json(res.locals[documentKey]);
 
-/** @type {<T> (options: { bind: (document: T, req: import("express").Request) => T | Promise<T> }) => import("express").RequestHandler} */
-export const bind = ({ bind }) => async (req, res, next) => {
+/**
+ * Assigns a new value to the document stored in the express locals after being processed by the given handler.
+ * @param {Object} options
+ * @param {(document: T, req: import("express").Request) => T | Promise<T>} options.handler The handler that bind the current document with the changes received in the request
+ * @param {string} [options.documentKey="document"] The express local key where the document is stored
+ * @returns {import("express").RequestHandler}
+ * @template T
+ * @example
+ * ```js
+ * router.route('/').post(bind({ handler: (_, req) => new Article(req.body) }));
+ * ```
+ * @example
+ * ```js
+ * router.param('id', load({ handler: (value) => findArticleById(value) }));
+ * router.route('/:id').put(bind({ handler: (document, req) => updateArticle(document, req.body) }));
+ * ```
+ * @example
+ * ```js
+ * router.param('id', load({ handler: (value) => findArticleById(value), documentKey: 'article' }));
+ * router.route('/:id').put(bind({ handler: (document, req) => updateArticle(document, req.body), documentKey: 'article' }));
+ * ```
+ */
+export const bind = ({ handler, documentKey = "document" }) => async (
+  req,
+  res,
+  next
+) => {
   try {
-    res.locals.document = await bind(res.locals.document, req);
+    res.locals[documentKey] = await handler(res.locals[documentKey], req);
     return next();
   } catch (error) {
     return next(error);
   }
 };
 
-/** @type {<T> (options: { validate: (document: T) => void | Promise<void> }) => import("express").RequestHandler} */
-export const validate = ({ validate }) => async (req, res, next) => {
+/**
+ * Validates the document stored in the express locals with the given handler.
+ * The handler should throw if the document is not valid.
+ * If the document is not valid, a 422 will be forwarded to error middlewares.
+ * @param {Object} options
+ * @param {(document: T) => any} options.handler The document validator handler. Should throw if the document is not valid.
+ * @param {string} [options.documentKey="document"] The express local key where the document is stored
+ * @returns {import("express").RequestHandler}
+ * @template T
+ * @example
+ * ```js
+ * router.route('/').post(
+ *   bind({ handler: (_, req) => new Article(req.body) }),
+ *   validate({ handler: (document) => validateArticle(document) })
+ * );
+ * ```
+ * @example
+ * ```js
+ * router.route('/').post(
+ *   bind({ handler: (_, req) => new Article(req.body), documentKey: 'article' }),
+ *   validate({ handler: (document) => validateArticle(document), documentKey: 'article' })
+ * );
+ * ```
+ * @example
+ * ```js
+ * router.param('id', load({ handler: (value) => findArticleById(value) }));
+ * router.route('/:id').put(
+ *   bind({ handler: (document, req) => updateArticle(document, req.body) }),
+ *   validate({ handler: (document) => validateArticle(document) })
+ * );
+ * ```
+ * @example
+ * ```js
+ * router.param('id', load({ handler: (value) => findArticleById(value), documentKey: 'article' }));
+ * router.route('/:id').put(
+ *   bind({ handler: (document, req) => updateArticle(document, req.body), documentKey: 'article' }),
+ *   validate({ handler: (document) => validateArticle(document), documentKey: 'article' })
+ * );
+ * ```
+ */
+export const validate = ({ handler, documentKey = "document" }) => async (
+  req,
+  res,
+  next
+) => {
   try {
-    await validate(res.locals.document);
+    await handler(res.locals[documentKey]);
     return next();
   } catch (error) {
     return next(Boom.badData(error.message, error));
   }
 };
 
-/** @type {<T> (options: { save: (document: T) => Promise<T>, isNew: boolean }) => import("express").RequestHandler} */
-export const save = ({ save, isNew }) => async (req, res, next) => {
+/**
+ * Saves the document stored in the express locals with the given handler.
+ * If `isNew` is specified, will return a 201 with the created document in the body.
+ * Otherwise, will return a 204.
+ * @param {Object} options
+ * @param {(document: T) => Promise<T>} options.handler The handler that saves the document
+ * @param {boolean} options.isNew Flag that determines the response (201 with created document or 204)
+ * @param {string} [options.documentKey="document"] The express local key where the document is stored
+ * @returns {import("express").RequestHandler}
+ * @template T
+ * @example
+ * ```js
+ * router.route('/').post(
+ *   bind({ handler: (_, req) => new Article(req.body) }),
+ *   validate({ handler: (document) => validateArticle(document) }),
+ *   save({ handler: (document) => saveArticle(document), isNew: true })
+ * );
+ * ```
+ * @example
+ * ```js
+ * router.route('/').post(
+ *   bind({ handler: (_, req) => new Article(req.body), documentKey: 'article' }),
+ *   validate({ handler: (document) => validateArticle(document), documentKey: 'article' }),
+ *   save({ handler: (document) => saveArticle(document), isNew: true, documentKey: 'article' })
+ * );
+ * ```
+ * @example
+ * ```js
+ * router.param('id', load({ handler: (value) => findArticleById(value) }));
+ * router.route('/:id').put(
+ *   bind({ handler: (document, req) => updateArticle(document, req.body) }),
+ *   validate({ handler: (document) => validateArticle(document) }),
+ *   save({ handler: (document) => saveArticle(document), isNew: false })
+ * );
+ * ```
+ * @example
+ * ```js
+ * router.param('id', load({ handler: (value) => findArticleById(value), documentKey: 'article' }));
+ * router.route('/:id').put(
+ *   bind({ handler: (document, req) => updateArticle(document, req.body), documentKey: 'article' }),
+ *   validate({ handler: (document) => validateArticle(document), documentKey: 'article' }),
+ *   save({ handler: (document) => saveArticle(document), isNew: false, documentKey: 'article' })
+ * );
+ * ```
+ */
+export const save = ({ handler, isNew, documentKey = "document" }) => async (
+  req,
+  res,
+  next
+) => {
   try {
-    const savedDocument = await save(res.locals.document);
+    const savedDocument = await handler(res.locals[documentKey]);
     return isNew ? res.status(201).json(savedDocument) : res.sendStatus(204);
   } catch (error) {
     return next(error);
   }
 };
 
-/** @type {<T> (options: { remove: (document: T) => any }) => import("express").RequestHandler} */
-export const remove = ({ remove }) => async (req, res, next) => {
+/**
+ * Saves the document stored in the express locals with the given handler.
+ * Will return a 204.
+ * @param {Object} options
+ * @param {(document: T) => any} options.handler The handler that removes the document.
+ * @param {string} [options.documentKey="document"] The express local key where the document is stored
+ * @returns {import("express").RequestHandler}
+ * @template T
+ * @example
+ * ```js
+ * router.param('id', load({ handler: (value) => findArticleById(value) }));
+ * router.route('/:id').delete(
+ *   remove({ handler: (document) => removeArticle(document) })
+ * );
+ * ```
+ * @example
+ * ```js
+ * router.param('id', load({ handler: (value) => findArticleById(value), documentKey: 'article' }));
+ * router.route('/:id').delete(
+ *   remove({ handler: (document) => removeArticle(document), documentKey: 'article' })
+ * );
+ * ```
+ */
+export const remove = ({ handler, documentKey = "document" }) => async (
+  req,
+  res,
+  next
+) => {
   try {
-    await remove(res.locals.document);
+    await handler(res.locals[documentKey]);
     return res.sendStatus(204);
   } catch (error) {
     return next(error);
