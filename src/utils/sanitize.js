aeba5e7978c9ea54e21863ba9dd4d2c9f25089f0
Antoine Colmard <a.colmard@gmail.com>
2020-09-20 15:33:44 +0200

    ♻ Avoid mutating input when sanitizing the request body

--- src/utils/sanitize.js
+++ src/utils/sanitize.js
@@ -1,6 +1,7 @@
-export const sanitize = (body) => {
-  delete body.createdAt;
-  delete body.updatedAt;
-  delete body.__v;
-  return body;
+/**
+ * Removes mongoose custom properties from the given object.
+ * @type {<T extends Object>(body: T) => Omit<T, ('createdAt' | 'updatedAt' | '__v')>}
+ */
+export const sanitize = ({ createdAt, updatedAt, __v, ...rest }) => {
+  return rest;
 };
